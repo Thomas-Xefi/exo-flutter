38257bdc6c21a7f915dc2b681573fd97e8652298
Thomas-Xefi <t.poirey@xefi.fr>
2022-10-25 11:29:29 +0200

    début avec getx

--- lib/widgets/exo4/getx_exo4_bindings.dart
+++ lib/widgets/exo4/getx_exo4_bindings.dart
@@ -1,1 +1,10 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:get/get.dart';
+
+class GetxExo4Bindings extends Bindings {
+  @override
+  void dependencies() {
+    // TODO: implement dependencies
+  }
+}
